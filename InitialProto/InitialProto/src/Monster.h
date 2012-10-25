b9a0b8565773dcd5ab57d18c2668b5f451b2c508
Eric Zhang <ezhang@ezhang-mac.(none)>
2012-10-24 21:34:54 -0500

    adding a blank class

---
+++ InitialProto/InitialProto/src/Monster.h
@@ -0,0 +1,13 @@
+//
+//  Monster.h
+//  InitialProto
+//
+//  Created by Eric Zhang on 10/24/12.
+//  Copyright (c) 2012 GutShotGames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Monster : NSObject
+
+@end
---
+++ InitialProto/InitialProto/src/Monster.m
@@ -0,0 +1,13 @@
+//
+//  Monster.m
+//  InitialProto
+//
+//  Created by Eric Zhang on 10/24/12.
+//  Copyright (c) 2012 GutShotGames. All rights reserved.
+//
+
+#import "Monster.h"
+
+@implementation Monster
+
+@end
